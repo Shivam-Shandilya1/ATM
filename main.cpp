580b999a3ac961cfbd48cfc5971d7a2ef7bf4929
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-03-07 09:55:36 +0530

    finishing

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 int main()
 {
+
+    int option,balance=50,deposit,withdraw;
+    do{
     cout<<"**************************************MENU****************************************************************"<<endl;
     cout<<"1.  BALANCE   "<<endl;
     cout<<"2.  DEPOSIT    "<<endl;
     cout<<"3.  WITHDRAWAL"<<endl;
     cout<<"4.  EXIT   "<<endl;
-    int option,balance=50,deposit,withdraw;
-    do{cout<<"Choose the following option number"<<endl;
+    cout<<"Choose the following option number"<<endl;
     cin>>option;
-
+     system("cls");
     switch(option)
     {case 1:cout<<"********************* BALANCE************************"<<endl;cout<<"You have "<<balance<<" Ruppees." <<endl;break;
     case 2: cout<<"******************************** DEPOSIT***********************"<<endl;
@@ -25,7 +27,7 @@ int main()
         balance=balance-withdraw;
         cout<<balance<<" Ruppees"<<endl;
     }else{cout<<"Invalid"<<endl;}break;
-    case 4:system("cls"); break;
+
     }
 }while(option!=4);
 return 0;
