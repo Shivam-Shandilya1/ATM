50f4da112249f08efb788fda6d60d2cfb3acbde9
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-03-07 09:45:17 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"**************************************MENU****************************************************************"<<endl;
+    cout<<"1.  BALANCE   "<<endl;
+    cout<<"2.  DEPOSIT    "<<endl;
+    cout<<"3.  WITHDRAWAL"<<endl;
+    cout<<"4.  EXIT   "<<endl;
+    int option,balance=50,deposit,withdraw;
+    do{cout<<"Choose the following option number"<<endl;
+    cin>>option;
+
+    switch(option)
+    {case 1:cout<<"********************* BALANCE************************"<<endl;cout<<"You have "<<balance<<" Ruppees." <<endl;break;
+    case 2: cout<<"******************************** DEPOSIT***********************"<<endl;
+    cin>>deposit;
+    balance=balance+deposit;
+    cout<<"You have "<<balance<<" Ruppees."<<endl;break;
+    case 3:cout<<"***************************WITHDRAWAL************************************"<<endl;
+    cout<<"You want to withdraw :";
+    cin >> withdraw;
+    if(withdraw<=balance)
+        { cout<<"Your withdraw is Successfull.Now Your Balance :-";
+        balance=balance-withdraw;
+        cout<<balance<<" Ruppees"<<endl;
+    }else{cout<<"Invalid"<<endl;}break;
+    case 4:system("cls"); break;
+    }
+}while(option!=4);
+return 0;
+}
